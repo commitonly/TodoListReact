63ba4d04f823e2f556593126ae1b6dbb023c327e
taemin <myloopmy@gmail.com>
2022-12-31 17:01:46 +0900

    추가하기 기능구현

--- todolist/src/components/TodoBoard.js
+++ todolist/src/components/TodoBoard.js
@@ -3,13 +3,18 @@ import TodoItem from "./TodoItem";
 
 function TodoBoard(props){
 
-    console.log("todoBoard", props.todoList)
+
+    
+
     return(
         <div>
             <h1>Todo List</h1>
-            <TodoItem/>
+            {
+            props.todoList.map((item)=>
+            <TodoItem item={item}/>)
+            }
         </div>
     )
 }
 
-export default TodoBoard
+export default TodoBoard 
--- todolist/src/components/TodoItem.js
+++ todolist/src/components/TodoItem.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function TodoItem(){
+function TodoItem(props){
 
     return(
         <div className="todo-item">
-            공부하기
+            {props.item}
         </div>
     )
 }
